1688831227641763a05db5b40e703961299d77f3
fshangala <fshangala@gmail.com>
2022-04-29 05:59:28 +0200

    installed pyside6

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from utils.Worker import FileWorker, DirectoryWorker
+from tkinter import *
+
+root = Tk()
+root.title('Shrinklare')
+
+root.mainloop()
 
 #worker = FileWorker('./test/t1.txt','./men')
 #print(worker.compress())
 
-worker2 = DirectoryWorker('./output_directory/test/', './original/')
-print(worker2.decompress())
+#worker2 = DirectoryWorker('./output_directory/test/', './original/')
+#print(worker2.decompress())
